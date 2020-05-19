cdc705915d2cab370537ff4cd50c54ee28eaa0f3
Dragan Opric <opric.dragan@gmail.com>
2020-05-19 07:44:10 +0200

    SeecData refactoring

--- Domain/SeedData.cs
+++ Domain/SeedData.cs
@@ -17,51 +17,42 @@ namespace timetobuy.Domain
                 if(context.Products.Any())
                 {
                     return;
+                } else
+                {
+                    InitilizeProducts(context);
                 }
-                context.Products
-                    .AddRange(new Product
-                    {
-                        Name = "First steps with React",
-                        Description = "no prior experiance needed",
-                        Price = 12.95M,
-                        Image = ""
-                    },
-                              new Product
-                    { Name = "Learning React", Description = "some description", Price = 29M, Image = "" },
-                              new Product
-                    {
-                        Name = "React advanced",
-                        Description = "ready to take next step?",
-                        Price = 35.5M,
-                        Image = ""
-                    },
-                              new Product
-                    {
-                        Name = "Asp.Net Core & React",
-                        Description = "Learn C#, WebApi and React",
-                        Price = 21.7M,
-                        Image = ""
-                    },
-                              new Product
-                    {
-                        Name = "React & Redux",
-                        Description = "React for enterprise applications",
-                        Price = 30M,
-                        Image = ""
-                    },
-                              new Product
-                    {
-                        Name = "SPA with React",
-                        Description = "Single Page Applications with React",
-                        Price = 45M,
-                        Image = ""
-                    },
-                              new Product
-                    {  Name = "Start your Vue Journey", Description = "lets learn vue", Price = 33M, Image = "" });
-
-                context.SaveChanges();
             }
-            ;
+        }
+
+        static void InitilizeProducts(DatabaseContext context)
+        {
+            context.Products
+                .AddRange(new Product
+                {
+                    Name = "First steps with React",
+                    Description = "no prior experiance needed",
+                    Price = 12.95M,
+                    Image = ""
+                },
+                          new Product
+                { Name = "Learning React", Description = "some description", Price = 29M, Image = "" },
+                          new Product
+                { Name = "React advanced", Description = "ready to take next step?", Price = 35.5M, Image = "" },
+                          new Product
+                { Name = "Asp.Net Core & React", Description = "Learn C#, WebApi and React", Price = 21.7M, Image = "" },
+                          new Product
+                { Name = "React & Redux", Description = "React for enterprise applications", Price = 30M, Image = "" },
+                          new Product
+                {
+                    Name = "SPA with React",
+                    Description = "Single Page Applications with React",
+                    Price = 45M,
+                    Image = ""
+                },
+                          new Product
+                { Name = "Start your Vue Journey", Description = "lets learn vue", Price = 33M, Image = "" });
+
+            context.SaveChanges();
         }
     }
 }
