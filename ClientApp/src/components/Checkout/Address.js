7d7ef80bc089e5b5666305c84bb113a9926166af
Dragan Opric <opric.dragan@gmail.com>
2020-05-29 08:22:02 +0200

    Address component created

--- ClientApp/src/components/Checkout/Address.js
+++ ClientApp/src/components/Checkout/Address.js
@@ -1,1 +1,57 @@
-﻿
+﻿import React, { Component } from 'react'
+import { Form, Label, FormGroup, Col, Input } from 'reactstrap'
+
+
+export default class Address extends Component {
+    state = {
+        street: '',
+        number: '',
+        zip: '',
+        city: ''
+    }
+
+    handleChange = (newState) => {
+        this.setState(newState, () => {
+            if (this.props.onAddressChanged) {
+                this.props.onAddressChanged(this.state)
+            }
+        })
+    }
+    render() {
+        return (
+
+            <Form>
+                <FormGroup row inline>
+                    <Label for="street" sm={2}>Street</Label>
+                    <Col sm={7}>
+                        <Input type="text" 
+                            id="street" placeholder="Street"
+                            value={this.state.street}
+                            onChange={(e) => this.handleChange({street: e.target.value})}/>
+                    </Col>
+                    <Label for="number" sm={1}>Number</Label>
+                    <Col sm={2}>
+                        <Input type="text" id="number" 
+                            value={this.state.number}
+                            onChange={(e) => this.handleChange({number: e.target.value})} />
+                        </Col>
+                </FormGroup>
+                <FormGroup row inline>
+                    <Label for="city" sm={2}>City</Label>
+                    <Col sm={7}>
+                        <Input type="text"
+                            id="city" placeholder="City"
+                            value={this.state.city}
+                            onChange={(e) => this.handleChange({ city: e.target.value })} />
+                    </Col>
+                    <Label for="zip" sm={1}>ZIP</Label>
+                    <Col sm={2}>
+                        <Input type="text" id="zip"
+                            value={this.state.zip}
+                            onChange={(e) => this.handleChange({ zip: e.target.value })} />
+                    </Col>
+                </FormGroup>
+            </Form>
+            )
+    }
+}
--- ClientApp/src/components/Checkout/Checkout.js
+++ ClientApp/src/components/Checkout/Checkout.js
@@ -2,16 +2,32 @@
 
 import React, { Component } from 'react'
 import CustomerDetails from './CustomerDetails'
+import Address from './Address'
+import { Input } from '@material-ui/core'
+import { Form, FormGroup, Label, Col } from 'reactstrap'
 
 export default class Checkout extends Component{
     state = {
         customer: {},
         billingAddress: {},
-        deliveryAddress: {}
+        deliveryAddress: {},
+        deliverToBillingAddress: true
     }
 
     onCustomerDetailsChanged = (details) => {
         this.setState({ customer: details })
+    }
+
+    onBillingAddressChanged = (newAddress) => {
+        this.setState({ billingAddress: newAddress })
+    }
+
+    onDeliveryAddressChanged = (newAddress) => {
+        this.setState({deliveryAddress: newAddress})
+    }
+
+    toggleChange = (e)=>{
+        this.setState({deliverToBillingAddress: !this.state.deliverToBillingAddress})
     }
 
     render() {
@@ -20,7 +36,25 @@ export default class Checkout extends Component{
                 <h4>Your Details</h4>
                 <CustomerDetails onChanged={this.onCustomerDetailsChanged}/>
                 <h4>Billing Address</h4>
-                <h4>Delivery Address</h4>
+                <Address onAddressChanged={this.onBillingAddressChanged} />
+
+                <Form>
+                    <FormGroup check>
+                        <Label check>
+                            <input type="checkbox" checked={this.state.deliverToBillingAddress}
+                                onChange={(e) => this.toggleChange(e)} />Delivery address same as billing address
+                           </Label>
+                        </FormGroup>
+                    </Form>
+               
+                {!this.state.deliverToBillingAddress &&
+                    <div>
+                    <h4>Delivery Address</h4>
+                    <Address onAddressChanged={this.onDeliveryAddressChanged} />
+                    </div>
+                }
+               
+                
             </div>
         )
     }
